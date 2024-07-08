0d2e2db915546b41aab3eb29317e05148d0e4a65
camkol <koliyus3@hotmail.com>
2024-07-08 11:34:41 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -30,6 +30,7 @@ const getComputerChoice = () => {
   }
 };
 const addResult = (result) => {
+  result.setAttribute;
   results.appendChild(result);
 };
 const determineWinner = (userChoice, computerChoice) => {
